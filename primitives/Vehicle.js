b1346d806a617a7bc922b2d94ec0d8d10910ba70
Pedro Costa <up201605339@fe.up.pt>
2018-11-23 19:29:12 +0000

    improved textures

--- primitives/Vehicle.js
+++ primitives/Vehicle.js
@@ -97,7 +97,7 @@ const base = [
 const bottom_body = [
 
 	[-0.2, 0, 10.5],
-	[-1.5, 0, 6.5],
+	[-1.65, 0, 6.5],
 	[-1.8, 0, 6.5],
 	[-1.8, 0, 3.5],
 	[-1.8, 0, 0],
@@ -116,7 +116,7 @@ const bottom_body = [
 
 
 	[0.2, 0, 10.5],
-	[1.5, 0, 6.5],
+	[1.65, 0, 6.5],
 	[1.8, 0, 6.5],
 	[1.8, 0, 3.5],
 	[1.8, 0, 0],
@@ -126,12 +126,12 @@ const bottom_body = [
 
 const bottom_cover = [
 
-		[-1.5, 0.0, 0],
-		[-0.1, 0, -5.5],
-		[0, 0, 0],
-		[0, 0, -5.5],
-		[1.5, 0.0, 0],
-		[0.1, 0.0, -5.5]
+	[-1.5, 0.0, 0],
+	[-0.1, 0, -5.5],
+	[0, 0, 0],
+	[0, 0, -5.5],
+	[1.5, 0.0, 0],
+	[0.1, 0.0, -5.5]
 
 ];
 
@@ -188,50 +188,59 @@ class Vehicle extends CGFobject {
 		this.thrusters = new Patch(this.scene, 3, 2, 20, 10, thrusters);
 		this.bot = new Patch(this.scene, 3, 7, 20, 10, bottom_body);
 		this.bottom_cover = new Patch(this.scene, 3, 2, 20, 10, bottom_cover);
-		this.arms = new MyCylinder(this.scene, 0.3,0.3,4,20,20); 
+		this.arms = new MyCylinder(this.scene, 0.3, 0.3, 4, 20, 20);
 
 		this.createMaterials();
 	};
 
-	createMaterials(){
-		this.scene.graph.materials['silver'].setTexture(this.scene.graph.textures['aircraft']);
-		
+	createMaterials() {
+
+		console.log(this.scene.graph.textures);
+
+		this.scene.graph.materials['silver'].setTexture(this.scene.graph.textures['fuselage']);
 
 		this.scene.graph.materials['beige'].setTexture(this.scene.graph.textures['guns_tubes']);
-		
 
+		this.scene.graph.materials['glass'].setTexture(this.scene.graph.textures['fuselage_window']);
 	}
 
 
 	display() {
 		this.scene.pushMatrix();
 
+		this.scene.scale(0.1, 0.1, 0.1);
+
+		this.scene.graph.materials['glass'].apply();
+		this.cockPitWindown.display();
 
 		this.scene.graph.materials['silver'].apply();
 
 		this.scene.gl.disable(this.scene.gl.CULL_FACE);
 
-		//this.scene.scale(0.1, 0.1, 0.1);
+		
 		this.body.display();
 		this.wingLeft.display();
 		this.wingRight.display();
 		this.upWing.display();
 		this.thrusters.display();
 		this.cockPit.display();
-		this.cockPitWindown.display();
 		this.bottom_cover.display();
+
+		
+
+		this.scene.graph.materials['silver'].apply();
+
 		this.scene.rotate(Math.PI, 0, 0, 1);
 		this.bot.display();
 
-
 		this.scene.graph.materials['beige'].apply();
-	
 
-			this.scene.translate(4, 0.3, 1); 
-			this.arms.display();
 
-			this.scene.translate(-8, 0, 0); 
-			this.arms.display();
+		this.scene.translate(4, 0.3, 1);
+		this.arms.display();
+
+		this.scene.translate(-8, 0, 0);
+		this.arms.display();
 
 		this.scene.gl.enable(this.scene.gl.CULL_FACE);
 

5d4d639efe81d57f01efae83e6faafa614606071
Pedro Costa <up201605339@fe.up.pt>
2018-11-24 23:08:58 +0000

    improved ship

--- primitives/Vehicle.js
+++ primitives/Vehicle.js
@@ -1,19 +1,24 @@
 const pitShell = [
 	[-1.5, 0, 6.5],
-	[-1.5, 0, 3.5],
+	[-1.2, 0, 3.5],
 	[0, 3.0, 6.5],
-	[0, 3.0, 3.5],
+	[0, 2.5, 3.5],
 	[1.5, 0, 6.5],
-	[1.5, 0, 3.5]
+	[1.2, 0, 3.5]
 ];
 
 const thrusters = [
-	[-1.5, 0.0, 0],
-	[-0.1, 0, -5.5],
+	[-0.8, 0.0, 0],
+	[-2.2, 0, -4],
+	[-1, 0, -4],
+
 	[0, 1.5, 0],
-	[0, 0, 0],
-	[1.5, 0.0, 0],
-	[0.1, 0.0, -5.5]
+	[0, 1.7, -2],
+	[0, 1.7, -4],
+
+	[0.8, 0.0, 0],
+	[2.2, 0.0, -4],
+	[1, 0.0, -4]
 ];
 
 const pitWindow = [
@@ -25,33 +30,34 @@ const pitWindow = [
 	[1.5, 0, 6.5]
 ];
 
-const body = [
-	[-1.5, 0, 3.5],
-	[-1.5, 0, 3],
-	[-1.5, 0, 2.5],
-	[-1.5, 0, 2],
-	[-1.5, 0, 1.5],
-	[-1.5, 0, 1],
-	[-1.5, 0, 0.5],
-	[-1.5, 0, 0],
 
-	[0, 3, 3.5],
-	[0, 2.95, 3],
-	[0, 2.9, 2.5],
-	[0, 2.6, 2],
-	[0, 2.2, 1.5],
+const body = [
+	[-1.2, 0, 3.5],
+	[-1.1, 0, 3],
+	[-1.0, 0, 2.5],
+	[-0.9, 0, 2],
+	[-0.8, 0, 1.5],
+	[-0.8, 0, 1],
+	[-0.8, 0, 0.5],
+	[-0.8, 0, 0],
+
+	[0, 2.5, 3.5],
+	[0, 2.4, 3],
+	[0, 2.3, 2.5],
+	[0, 2.2, 2],
+	[0, 2.1, 1.5],
 	[0, 1.8, 1],
 	[0, 1.6, 0.5],
 	[0, 1.5, 0],
 
-	[1.5, 0, 3.5],
-	[1.5, 0, 3],
-	[1.5, 0, 2.5],
-	[1.5, 0, 2],
-	[1.5, 0, 1.5],
-	[1.5, 0, 1],
-	[1.5, 0, 0.5],
-	[1.5, 0, 0]
+	[1.2, 0, 3.5],
+	[1.1, 0, 3],
+	[1.0, 0, 2.5],
+	[0.9, 0, 2],
+	[0.8, 0, 1.5],
+	[0.8, 0, 1],
+	[0.8, 0, 0.5],
+	[0.8, 0, 0]
 ];
 
 const pitBack = [
@@ -78,7 +84,7 @@ const bottom_body = [
 	[-1.8, 0, 3.5],
 	[-1.8, 0, 0],
 	[-1.5, 0, 0],
-	[-0.1, 0, -5.5],
+	[-0.1, 0, -3.5],
 
 	[0, 0, 10.5],
 	[0, 3.0, 3.5],
@@ -86,7 +92,7 @@ const bottom_body = [
 	[0, 3, 3.5],
 	[0, 3, 0],
 	[0, 3, 0],
-	[0, 0, -5.5],
+	[0, 0, -3.5],
 
 	[0.2, 0, 10.5],
 	[1.65, 0, 6.5],
@@ -94,17 +100,17 @@ const bottom_body = [
 	[1.8, 0, 3.5],
 	[1.8, 0, 0],
 	[1.5, 0, 0],
-	[0.1, 0.0, -5.5]
+	[0.1, 0.0, -3.5]
 ];
 
 const bottom_cover = [
 
 	[-1.5, 0.0, 0],
-	[-0.1, 0, -5.5],
+	[-0.1, 0, -3.5],
 	[0, 0, 0],
-	[0, 0, -5.5],
+	[0, 0, -3.5],
 	[1.5, 0.0, 0],
-	[0.1, 0.0, -5.5]
+	[0.1, 0.0, -3.5]
 
 ];
 
@@ -112,13 +118,13 @@ const wingRight = [
 	[-7, 0, 1],
 	[-7, 0, -1],
 	[-1.5, 0, 6],
-	[-1.5, 0, 0]
+	[0, 0, 0]
 
 ];
 
 const wingLeft = [
 	[1.5, 0, 6],
-	[1.5, 0, 0],
+	[0, 0, 0],
 	[7, 0, 1],
 	[7, 0, -1]
 ];
@@ -147,10 +153,12 @@ class Vehicle extends CGFobject {
 		this.wingRight = new Patch(this.scene, 2, 2, 20, 10, wingRight);
 		this.upWing = new Patch(this.scene, 2, 2, 20, 10, upWing);
 		this.body = new Patch(this.scene, 3, 8, 20, 10, body);
-		this.thrusters = new Patch(this.scene, 3, 2, 20, 10, thrusters);
+		this.thrusters = new Patch(this.scene, 3, 3, 20, 10, thrusters);
 		this.bot = new Patch(this.scene, 3, 7, 20, 10, bottom_body);
 		this.bottom_cover = new Patch(this.scene, 3, 2, 20, 10, bottom_cover);
 		this.arms = new MyCylinder(this.scene, 0.3, 0.3, 4, 20, 20);
+
+		this.exhaust = new MyCylinder(this.scene, 0.43, 0.3, 2, 20, 20);
 
 		this.createMaterials();
 	};
@@ -164,6 +172,8 @@ class Vehicle extends CGFobject {
 		this.scene.graph.materials['beige'].setTexture(this.scene.graph.textures['guns_tubes']);
 
 		this.scene.graph.materials['glass'].setTexture(this.scene.graph.textures['fuselage_window']);
+	
+		this.scene.graph.materials['fire_mat'].setTexture(this.scene.graph.textures['fire']);
 	}
 
 	/**
@@ -183,7 +193,18 @@ class Vehicle extends CGFobject {
 		this.wingLeft.display();
 		this.wingRight.display();
 		this.upWing.display();
+
+
+		
+
+		this.scene.rotate(-Math.PI, 0, 0, 1);
 		this.thrusters.display();
+
+		
+
+		this.scene.rotate(-Math.PI, 0, 0, 1);
+		this.thrusters.display();
+
 		this.cockPit.display();
 		this.bottom_cover.display();
 
@@ -196,6 +217,15 @@ class Vehicle extends CGFobject {
 		this.arms.display();
 		this.scene.translate(-8, 0, 0);
 		this.arms.display();
+
+
+		this.scene.graph.materials['fire_mat'].apply();
+		this.scene.translate(3.6, -0.35, -5);
+		this.exhaust.display();
+
+		this.scene.translate(0.9, 0, 0);
+		this.exhaust.display();
+
 
 		this.scene.gl.enable(this.scene.gl.CULL_FACE);
 
